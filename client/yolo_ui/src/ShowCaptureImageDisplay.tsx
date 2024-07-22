68927daf1e1a05dacc0a5b44f0d63a57a4e4319e
Beh Chun Siang <100542890+ChunSiang-1109@users.noreply.github.com>
2024-07-22 11:00:15 +0800

    Delete client/yolo_ui/src/ShowCaptureImageDisplay.tsx

--- client/yolo_ui/src/ShowCaptureImageDisplay.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {  RefObject } from 'react';
-
-export default function ShowCaptureImageDisplay({image}: {image: RefObject<HTMLCanvasElement>}) {
-  return (
-    <React.Fragment>
-      <canvas ref={image}   style={{ width: '100%', height: 'auto' }}/>
-    </React.Fragment>);
-}
